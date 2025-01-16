61ab7fdfa06f0c37525ceac0fc08f85d10d7dc37
곽병혁 <aiden-kwak@gwagbyeonghyeog-ui-MacBookPro.local>
2025-01-16 21:53:37 +0900

    threeJS 렌더링 적용, 모델후처리완료

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import MainForm from "@/components/mainForm";
+
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <p className={styles.font1}>폰트 테스트. 동해물과 백두산이 마르고 닳도록.</p>
-        <p className={styles.font2}>Rêve 폰트 테스트(font test). 동해물과 백두산이 마르고 닳도록.</p>
+        <MainForm/>
       </main>
       <footer className={styles.footer}>
         <p className="logo-main-style">Rêve</p>
