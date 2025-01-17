dff36cf500da1fb492e9b16fa68ecc5dbed51d3f
곽병혁 <aiden-kwak@gwagbyeonghyeog-ui-MacBookPro.local>
2025-01-18 05:34:11 +0900

    footer mobile

--- frontend/src/components/footerForm.jsx
+++ frontend/src/components/footerForm.jsx
@@ -3,8 +3,8 @@ import styles from './FooterForm.module.css'
 export default function FooterForm () {
     return (
         <footer className={styles.footer}>
-            <p className="logo-main-style">Rêve</p>
-            <p>COPYRIGHT (C) @Aiden-Kwak. All Rights reserved</p>
+            <p className={`${styles.mobileLogo} logo-main-style`}>Rêve</p>
+            <p className={styles.mobile}>COPYRIGHT (C) @Aiden-Kwak. All Rights reserved</p>
         </footer>
     );
 }
