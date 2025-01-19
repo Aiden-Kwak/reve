5de711c3c9dfae882edc5055ad3eb3a5765b06df
곽병혁 <aiden-kwak@gwagbyeonghyeog-ui-MacBookPro.local>
2025-01-19 10:17:29 +0900

    /api로 못넘기는 문제 예상: ngnix 프록시문제/ admin도 처리함

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
           source: "/media/:path*/",
           destination: process.env.NEXT_PUBLIC_API_SERVER_URL + "/media/:path*/",
         },
+        {
+          source: "/admin/:path*/",
+          destination: process.env.NEXT_PUBLIC_API_SERVER_URL + "/admin/:path*/",
+        },
       ];
     },
     trailingSlash: true,
